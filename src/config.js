c1839054f1a95b43f780d50d847322ce5ca74beb
EDISON FERNEY VILLARREAL CARDENAS <tu-nuevo-correo@example.com>
2025-04-22 23:07:46 -0500

    Update Presenter y Repository for Pay Mode

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ mysql: {
     PORT: process.env.MYSQL_PORT || 3306,
     user: process.env.MYSQL_USER || "root",
     password: process.env.MYSQL_PASSWORD || "",
-    database: process.env.MYSQL_DATABASE || "practica1",
+    database: process.env.MYSQL_DATABASE || "parcial_2",
 },
 
 };
